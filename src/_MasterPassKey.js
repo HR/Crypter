4d566b2f53b2b6dd4c714c76bab5f265c72104d7
Habib R <Habib.R@outlook.com>
2016-04-17 18:20:36 +0100

    finalise release of v1

--- src/_MasterPassKey.js
+++ src/_MasterPassKey.js
@@ -4,17 +4,6 @@
  * Provides a way to securely set and retrieve the MasterPass globally
  ******************************/
 
-// private static class member
-// var MP = Symbol()
-// module.exports = class MasterPassKey {
-// 	static get() {
-// 		return this[MP]
-// 	}
-// 	static set(mp) {
-// 		this[MP] = mp
-// 	}
-// }
-
 const MasterPassKey = (function () {
   const mpk = new WeakMap()
 
