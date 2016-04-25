d06a82b03c2912dd598aa7708d7f118271e4111e
Habib R <Habib.R@outlook.com>
2016-04-25 17:27:37 +0100

    fix issue and make tests pass

--- src/crypto.js
+++ src/crypto.js
@@ -6,7 +6,6 @@
 
 const secrets = require('secrets.js')
 const fs = require('fs-extra')
-const util = require('./util')
 const path = require('path')
 const Readable = require('stream').Readable
 const scrypto = require('crypto')
