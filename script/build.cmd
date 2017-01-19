7dc46281bffec25f8beeeb77d64c69c52f931d89
Habib R <Habib.R@outlook.com>
2017-01-19 22:37:44 +0000

    Crypter 3.0.0 release [build]

--- script/build.cmd
+++ script/build.cmd
@@ -1,9 +1,8 @@
 echo Building Crypter
 cd %APPVEYOR_BUILD_FOLDER%
 set NODE_ENV=production
-npm install electron-builder@next -g
 npm install --production
 npm prune
 node --version
 npm --version
-build -w --x64
+npm run winbuild
