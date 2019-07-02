3ea47b48a18145a677c805d0116e91caa41ee1d8
HR <h@rehman.email>
2019-07-02 21:57:14 +0100

    try using docker to [build] for linux

--- script/build_ml.sh
+++ script/build_ml.sh
@@ -12,10 +12,14 @@ npm prune
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   # to build for linux
-  sudo apt-get install --no-install-recommends -y icnsutils graphicsmagick xz-utils
-  sudo snap install snapcraft --classic
   echo "Building for linux"
-  npm run build:lin
+  docker run --rm \
+        --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
+        -v ${PWD}:/project \
+        -v ~/.cache/electron:/root/.cache/electron \
+        -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+        electronuserland/builder:wine \
+        /bin/bash -c "npm run build:lin"
 else
   echo "Building for mac"
   npm run build:mac
